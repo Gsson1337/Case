c75d5b91b2b79dd9d0c8149d7ff0afe7ae41a1d7
Johan <johangust1@gmail.com>
2022-11-23 19:27:54 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,72 @@
+import requests
+#import json
+
+def main():
+    api_key = "e00f1c5f8dcfd9423593c096a8212536"
+ 
+    # base_url variable to store url
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    
+    # Give city name
+    city_name = input("Enter city name : ")
+    #city_name = "gothenburg"
+    
+    # complete_url variable to store
+    # complete url address
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name + "&units=metric" + "&lang=sv"
+    
+    # get method of requests module
+    # return response object
+    response = requests.get(complete_url)
+    
+    # json method of response object
+    # convert json format data into
+    # python format data
+    x = response.json()
+
+    # Now x contains list of nested dictionaries
+    # Check the value of "cod" key is equal to
+    # "404", means city is found otherwise,
+    # city is not found
+    if x["cod"] != "404":
+    
+        # store the value of "main"
+        # key in variable y
+        y = x["main"]
+    
+        # store the value corresponding
+        # to the "temp" key of y
+        current_temperature = y["temp"]
+    
+        # store the value corresponding
+        # to the "pressure" key of y
+        current_pressure = y["pressure"]
+    
+        # store the value corresponding
+        # to the "humidity" key of y
+        current_humidity = y["humidity"]
+    
+        # store the value of "weather"
+        # key in variable z
+        z = x["weather"]
+    
+        # store the value corresponding
+        # to the "description" key at
+        # the 0th index of z
+        weather_description = z[0]["description"]
+    
+        # print following values
+        print(" Temperature (in Celsius unit) = " +
+                        str(current_temperature) + chr(176) +
+            "\n atmospheric pressure (in hPa unit) = " +
+                        str(current_pressure) +
+            "\n humidity (in percentage) = " +
+                        str(current_humidity) +
+            "\n description = " +
+                        str(weather_description))
+    
+    else:
+        print(" City Not Found ")
+
+if __name__ == "__main__":
+    main()
